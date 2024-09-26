c5b6f5d92c647bc17fdc843db53cf7773b3bbb96
Fábio Filho <fabiopmfilho@outlook.com>
2024-09-26 12:21:01 -0300

    feat: add functionality to ord and sort buttons

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,35 +1,31 @@
-import { Product } from "./Product";
-
 const serverUrl = "http://localhost:5000";
 
 function main() {
   console.log(serverUrl);
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const filterButton = document.querySelector('.btn_filter') as HTMLButtonElement;
-    const sortButton = document.querySelector('.btn_sort') as HTMLButtonElement;
-    const filterMenu = document.querySelector('.filter_menu') as HTMLDivElement;
-    const sortMenu = document.querySelector('.sort_menu') as HTMLDivElement;
-    const closeButtons = document.querySelectorAll('.close-menu') as NodeListOf<HTMLButtonElement>;
-  
-    filterButton?.addEventListener('click', () => {
-      filterMenu?.classList.add('active');
-      console.log("Filter")
-    });
-    
-    sortButton?.addEventListener('click', () => {
-      sortMenu?.classList.add('active');
-      console.log("Sort")
-    });
-  
-    closeButtons.forEach(button => {
-      button.addEventListener('click', () => {
-        filterMenu?.classList.remove('active');
-        sortMenu?.classList.remove('active');
-      });
+  const filterButton = document.querySelector('.btn_filter') as HTMLButtonElement;
+  const sortButton = document.querySelector('.btn_sort') as HTMLButtonElement;
+  const filterMenu = document.querySelector('.menu_overlay.filter_menu') as HTMLDivElement;
+  const sortMenu = document.querySelector('.menu_overlay.sort_menu') as HTMLDivElement;
+  const closeButtons = document.querySelectorAll('.close_menu') as NodeListOf<HTMLButtonElement>;
+
+  filterButton?.addEventListener('click', () => {
+    filterMenu?.classList.add('active');
+    console.log("Filter opened");
+  });
+
+  sortButton?.addEventListener('click', () => {
+    sortMenu?.classList.add('active');
+    console.log("Sort opened");
+  });
+
+  closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      filterMenu?.classList.remove('active');
+      sortMenu?.classList.remove('active');
+      console.log("Menus closed");
     });
   });
-  
 }
 
 document.addEventListener("DOMContentLoaded", main);
